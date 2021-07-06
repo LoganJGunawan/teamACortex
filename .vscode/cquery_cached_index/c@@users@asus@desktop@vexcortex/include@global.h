bc453b3a1804b7fb1647223e2a46f591bfca51a2
LoganG <logan.j.gunawan3@gmail.com>
2021-07-06 11:10:12 +1000

    Fresh start for cortex coding

---
+++ .vscode/cquery_cached_index/c@@users@asus@desktop@vexcortex/include@global.h
@@ -0,0 +1,7 @@
+#include "main.h"
+extern pros::Motor rightFront(1);
+extern pros::Motor rightBack(2);
+extern pros::Motor leftFront(3);
+extern pros::Motor leftBack(4);
+extern pros::Motor hook(5);
+extern int maxMotorSpeed=1000;
