8599d64ea86160d9d17a411218af9697ea7e8bb9
tulucat <tulumaw@gmail.com>
2021-07-06 13:48:36 +1000

    initial commit

--- .vscode/cquery_cached_index/c@@users@asus@desktop@vexcortex/include@global.h
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-extern pros::Motor rightFront(1);
-extern pros::Motor rightBack(2);
-extern pros::Motor leftFront(3);
-extern pros::Motor leftBack(4);
-extern pros::Motor hook(5);
-extern int maxMotorSpeed=1000;
